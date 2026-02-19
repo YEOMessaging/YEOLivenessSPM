af98f94a6caab7640b5a17bf4a0ef8cb456695f5
paul calver <thecalver@gmail.com>
2026-02-19 13:59:04 +0000

    YEOLiveness 0.2.0 – update URL + checksum

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "YEOLiveness",
-      url: "https://github.com/YEOMessaging/YEOLivenessSPM/releases/download/0.1.4/YEOLiveness.xcframework.zip",
-      checksum: "62685a8227452dd30b3530dac4ed44760a9e5fb0842e5c341b2e1ff438dadcbd"
+      url: "https://github.com/YEOMessaging/YEOLivenessSPM/releases/download/0.2.0/YEOLiveness.xcframework.zip",
+      checksum: "58fb03fffb532e4c51735d55d2945a80bb4776001f1818e7d50f65c97fd5b4d0"
     )
   ]
 )
